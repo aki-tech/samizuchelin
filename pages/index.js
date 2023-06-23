ed33e30567b80cfc200fe95255e2221d51070ff8
aki-tech <gogoakito666@gmail.com>
2023-06-23 09:39:34 +0900

    開発用環境の整備

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 
 export default function Home() {
   return (
-    <div>samizuchelin ,,,</div>
+    <>
+      <header>
+        <div>SAMIZUCHELIN2023</div>
+        <div>HOW TO</div>
+        <div>SAMIZUCHELIN</div>
+        <div>SAMIZU OF THE YEAR</div>
+      </header>
+      <contents>
+        <div>
+          ここにコンテンツが入ります。
+        </div>
+      </contents>
+    </>
   )
 }
